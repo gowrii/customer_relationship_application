d0591a3cbd10e987c47ecc5448d04ad62175e2f8
Gowri Nambiar <gowri.nambi@gmail.com>
2013-10-27 22:15:56 -0400

    modify contacts changed

--- rolodex.rb
+++ rolodex.rb
@@ -19,45 +19,44 @@ class Rolodex #just for dev
     @contacts = contacts
   end
 
+
+
+
+
   def self.modify_contact
-    customer_input = gets.chomp
-    @contacts.each do |contact|
-      if customer_input == (contact.first_name || contact.last_name || contact.e_mail || contact.notes)
-          puts "#{contact.first_name} #{contact.last_name}"
-          puts "#{contact.e_mail}"
-          puts "#{contact.notes}"
-      else
-        "empty"
-      end
-      end
-      puts "Is this the contact you want to edit Y/N?"
-      answer = gets.chomp
-      if answer = "Y".capitalize
-      else
-      exit 
-      end
-    @contacts.each do |contact|
-      if customer_input == (contact.first_name || contact.last_name || contact.e_mail || contact.notes)
-        @contacts.delete(contact)
-      end
-      self.updating_contact
-    end
-    
-  end
-  
-  def self.updating_contact
-    print "What is your first name: "
-    first_name = gets.chomp
-    print "What is your last name: "
-    last_name = gets.chomp
-    print "What is your e-mail address: "
-    e_mail = gets.chomp
-    print "Add a note: "
-    notes = gets.chomp
-    puts "-------------------------------------------"
+  customer_input = gets.chomp
+  @contacts.each { |contact| puts contact.display_values if customer_input == (contact.first_name || contact.last_name || contact.e_mail || contact.notes)}
+  @contacts.each {|contact| @contacts.delete(contact) if customer_input == (contact.first_name || contact.last_name || contact.e_mail || contact.notes)}
+  #puts "Do you want to change contact? yes/no"
+  #answer = gets.chomp
+  #if answer == 'yes'
+    puts "Enter new first name"
+      first_name = gets.chomp
+    puts "Enter new last name"
+      last_name = gets.chomp
+    puts "Enter new e-mail"
+      e_mail = gets.chomp
+    puts "Enter new note"
+      notes = gets.chomp
     contact = Contact.new(first_name, last_name, e_mail, notes)
     Rolodex.adding_contact(contact)
+  
+  #end
+
+  
+  
+
+
+
+
   end
+ 
+
+
+
+
+
+
 
   def self.display_all_contact
       @contacts.each do |contact| contact.display_values
@@ -75,13 +74,16 @@ class Rolodex #just for dev
   end
   
 
-  def self.delete_contact
+  def self.delete_contact ()
     customer_input = gets.chomp
+    case customer_input
+    when 
       @contacts.each do |contact| @contacts.delete(contact) if customer_input == (contact.first_name || contact.last_name || contact.e_mail || contact.notes)
       puts "Your contact #{contact.first_name} #{contact.last_name} has been deleted"
       puts "Returning to main menu"
       puts " "
       end
+    end
   end
 end
 
